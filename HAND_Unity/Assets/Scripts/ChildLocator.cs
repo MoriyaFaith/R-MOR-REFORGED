8cf9e722db8b50bdf9d850887e0c0d0b25983bf8
Moffein <22742925+Moffein@users.noreply.github.com>
2022-10-27 02:29:31 -0700

    HAN-D model is in-game. Todo: - Fix ChildLocator error spam. - Get anims working. - Fix missing parts on HAN-D's model.

---
+++ HAND_Unity/Assets/Scripts/ChildLocator.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System;
+
+public class ChildLocator : MonoBehaviour
+{
+	[Serializable]
+	public struct NameTransformPair
+	{
+		public string name;
+		public Transform transform;
+	}
+
+	[SerializeField]
+	private NameTransformPair[] transformPairs;
+
+    public NameTransformPair[] TransformPairs { get => transformPairs; set => transformPairs = value; }
+}
