d51ce160ce20aff9288b74f720db5804d260f6fa
Moffein <22742925+Moffein@users.noreply.github.com>
2022-11-05 20:03:50 -0700

    Cannon primary.

---
+++ HenryMod/Content/RMORSurvivor/SkillStates/Primary/ChargeCannon.cs
@@ -0,0 +1,67 @@
+﻿using RoR2;
+using UnityEngine;
+
+namespace EntityStates.RMOR.Primary
+{
+    public class ChargeCannon : BaseState
+    {
+        public static float baseDuration;
+        public static string partialChargeSoundString;
+        public static string fullChargeSoundString;
+        public static GameObject partialChargeEffect;
+        public static GameObject fullChargeEffect;
+        public static int maxChargeLevel = 3;
+
+        private float duration;
+        public int chargeLevel;
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            chargeLevel = 0;
+            this.duration = ChargeCannon.baseDuration / this.attackSpeedStat;
+        }
+
+        public override void FixedUpdate()
+        {
+            base.FixedUpdate();
+
+            if (chargeLevel < ChargeCannon.maxChargeLevel && base.fixedAge >= (chargeLevel + 1) * duration / (float)ChargeCannon.maxChargeLevel)
+            {
+                chargeLevel++;
+                string soundString = ChargeCannon.partialChargeSoundString;
+                GameObject effectPrefab = ChargeCannon.partialChargeEffect;
+                if (chargeLevel >= ChargeCannon.maxChargeLevel)
+                {
+                    soundString = ChargeCannon.fullChargeSoundString;
+                    effectPrefab = ChargeCannon.fullChargeEffect;
+                }
+                Util.PlaySound(soundString, base.gameObject);
+                EffectManager.SimpleMuzzleFlash(effectPrefab, base.gameObject, "HandL", false);
+                EffectManager.SimpleMuzzleFlash(effectPrefab, base.gameObject, "HandR", false);
+            }
+
+            if (base.isAuthority)
+            {
+                if (!base.inputBank || !base.inputBank.skill1.down)
+                {
+                    if (chargeLevel > 0)
+                    {
+                        SetNextState();
+                        return;
+                    }
+                }
+            }
+        }
+
+        public override InterruptPriority GetMinimumInterruptPriority()
+        {
+            return InterruptPriority.Skill;
+        }
+
+        public virtual void SetNextState()
+        {
+            this.outer.SetNextState(new FireCannon() { chargeLevel = this.chargeLevel });
+        }
+    }
+}
---
+++ HenryMod/Content/RMORSurvivor/SkillStates/Primary/FireCannon.cs
@@ -0,0 +1,80 @@
+﻿using RoR2;
+using RoR2.Projectile;
+using UnityEngine;
+
+namespace EntityStates.RMOR.Primary
+{
+    public class FireCannon : BaseState
+    {
+        public static string attackSoundString;
+        public static GameObject projectilePrefab;
+        public static float baseExitDuration = 0.6f;
+        public static float baseDurationBetweenShots = 0.2f;
+        public static float damageCoefficient = 5f;
+        public static float force = 2000f;
+        public static GameObject muzzleflashEffectPrefab;
+
+        public int chargeLevel;
+        private bool crit;
+
+        private float totalDuration;
+        private float durationBetweenShots;
+        private float fireStopwatch;
+        private int shotsRemaining;
+
+        public override InterruptPriority GetMinimumInterruptPriority()
+        {
+            return InterruptPriority.PrioritySkill;
+        }
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            shotsRemaining = chargeLevel;
+            crit = base.RollCrit();
+            fireStopwatch = 0f;
+            durationBetweenShots = FireCannon.baseDurationBetweenShots / this.attackSpeedStat;
+            totalDuration = FireCannon.baseExitDuration / this.attackSpeedStat + durationBetweenShots * shotsRemaining;
+
+            if (shotsRemaining > 0)
+            {
+                FireProjectile();
+            }
+        }
+
+        public override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            if (shotsRemaining > 0)
+            {
+                fireStopwatch += Time.fixedDeltaTime;
+                if (fireStopwatch >= durationBetweenShots)
+                {
+                    FireProjectile();
+                    fireStopwatch -= durationBetweenShots;
+                }
+            }
+            else
+            {
+                if (base.isAuthority && base.fixedAge > totalDuration)
+                {
+                    this.outer.SetNextStateToMain();
+                    return;
+                }
+            }
+        }
+
+        private void FireProjectile()
+        {
+            shotsRemaining--;
+            Util.PlaySound(FireCannon.attackSoundString, base.gameObject);
+            EffectManager.SimpleMuzzleFlash(FireCannon.muzzleflashEffectPrefab, base.gameObject, "HandL", false);
+            EffectManager.SimpleMuzzleFlash(FireCannon.muzzleflashEffectPrefab, base.gameObject, "HandR", false);
+            Ray aimRay = base.GetAimRay();
+            if (base.isAuthority)
+            {
+                ProjectileManager.instance.FireProjectile(FireCannon.projectilePrefab, aimRay.origin, Util.QuaternionSafeLookRotation(aimRay.direction), base.gameObject, this.damageStat * FireCannon.damageCoefficient, FireCannon.force, crit);
+            }
+        }
+    }
+}
