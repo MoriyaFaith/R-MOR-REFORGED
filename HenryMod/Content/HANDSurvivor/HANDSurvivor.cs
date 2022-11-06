8b3efc860da11b1b97da8996b68abaff41a16848
Moffein <22742925+Moffein@users.noreply.github.com>
2022-11-05 17:40:12 -0700

    Attempted fix for head not turning (didn't work).

--- HenryMod/Content/HANDSurvivor/HANDSurvivor.cs
+++ HenryMod/Content/HANDSurvivor/HANDSurvivor.cs
@@ -76,6 +76,11 @@ namespace HANDMod.Content.HANDSurvivor
             Transform fistHitboxTransform = childLocator.FindChild("FistHitbox");
             Prefabs.SetupHitbox(model, "FistHitbox", new Transform[] { fistHitboxTransform });
 
+            /*AimAnimator aan = model.GetComponent<AimAnimator>();
+            aan.yawRangeMin = -180f;
+            aan.yawRangeMax = 180f;
+            aan.fullYaw = true;*/
+
             Transform chargeHammerHitboxTransform = childLocator.FindChild("ChargeHammerHitbox");
             Prefabs.SetupHitbox(model, "ChargeHammerHitbox", new Transform[] { chargeHammerHitboxTransform });
 
